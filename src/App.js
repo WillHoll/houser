4d24b1aef4e1dd61e48971bcf91166582ef927dd
WillHoll <willhollingsworth15@yahoo.com>
2019-11-08 10:28:40 -0700

    begin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Wizard from './components/Wizard/Wizard';
+import House from './components/House/House';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Wizard/>
+      <House/>
+      <Dashboard/>
     </div>
   );
 }
