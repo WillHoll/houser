9c9729a9fc8bfd3f2d7b512afa4cff9e8715df48
WillHoll <willhollingsworth15@yahoo.com>
2019-11-08 11:01:18 -0700

    setup complete; begin work

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env;
+const ctrl = require('./controller');
+
+const app = express();
+
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    console.log('Database connected')
+    app.listen(SERVER_PORT, () => console.log(`MY POWER! IT'S OVER ${SERVER_PORT}`))
+})
